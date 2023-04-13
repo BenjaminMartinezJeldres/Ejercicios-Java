d2078d5cde342800b78796d4fff1d2665ee7e396
Benjamín Martinez <ben.martinezj@gmail.com>
2023-04-13 13:21:30 -0400

    agregado de Public Class Main

---
+++ basico/basico.java
@@ -0,0 +1,92 @@
+p
+Public class Main{
+
+
+
+
+    public static void variables() {
+
+    //------------------Boleanos---------------//
+boolean a = true;
+boolean b= false;
+
+
+
+//--------------caracteres------------------//
+char a = 's';
+char b = '1';
+char c = '\u00C1';//*usando slash al reves */
+
+//--------------------enteros-------------------------------------//
+
+byte enteroByte =127;//tamaño 1 byte
+
+short eneteroShort = 32767;//tamaño 2 bytes
+
+int enteroInt = 2147483647;//tamaño 4 bytes
+
+long enteroLong = 9223372036436434634636; //tamaño 8 bytes
+
+//--------------Flotantes-----------------------------//
+
+float numeroFloat = 3.1426f; //tamaño 4 bytes
+
+double numeroDouble= 3.1416; // tamaño 8 bytes
+
+//--------------------Operadores Aritmeticos------------------//
+/*
+Multiplicacion: *
+Division: /
+Modulo: %
+Suma: +
+Resta: -
+ */
+//-------------------Operadores de Incremento y Decremento------//
+
+/*
+Pre-incremento: ++a
+Post-incremento: a++
+Pre-decremento:--a
+Post-decremento: a--
+
+ */
+
+
+ //------------------Operadores Relacionales-------------------//
+/*
+Mayor que: >
+Mayor o igual que:>=
+Menor que: <
+Menor o igual que:<=
+Es igual que: ==
+Es distinto de: !=
+  */
+
+
+  //------------------Operadores Logicos-------------------//
+/*
+▪ And: &&
+▪ Or: ||
+▪ Not:!
+▪ And: &
+▪ Or: |
+  */
+
+  //------------------Sentencias de Control-------------------//
+/*
+▪ if-else
+▪ switch-case
+▪ for
+▪ while
+▪ do-while
+▪ foreach
+  */
+
+
+  //------------------String-------------------//
+String variable = "texto del string"; //almacena secuencia de Caracteres
+
+}
+
+
+}
